7913e776773817bb6d25e8a78ebc7e0507913ebb
gaburieru-dev <gremista.dariva@gmail.com>
2020-10-26 15:54:52 -0300

    Aula 21/09

--- mvc-pg/routes/index.js
+++ mvc-pg/routes/index.js
@@ -5,15 +5,35 @@ const routes = new express.Router();
 const cidadeRoute = require('./cidade.route');
 const pessoaRoute = require('./pessoa.route');
 const usuarioRoute = require('./usuario.route');
-const curtidaRoute = require('./curtida.route');
+
 const fotoRoute = require('./foto.route');
+const curtidaRoute = require('./curtida.route');
 const comentarioRoute = require('./comentario.route');
+const perfilRoute = require('./perfil.route');
+const feedRoute = require('./feed.route');
+const seguidoRoute = require('./seguido.route');
+
+routes.param('username', (req, res, next, username) => {
+    //Capturando o username da requisição e adicionando ao Request
+    req.username = username;
+
+    //Continuar a requisição
+    next();
+});
+
 
 routes.use('/cidades', cidadeRoute);
 routes.use('/pessoas', pessoaRoute);
 routes.use('/account', usuarioRoute);
-routes.use('/:username/fotos', fotoRoute);
+
 routes.use('/curtidas', curtidaRoute);
 routes.use('/comentarios', comentarioRoute);
+
+routes.use('/:username/fotos', fotoRoute);
+routes.use('/:username/perfil', perfilRoute);
+
+routes.use('/:username/feed', feedRoute);
+routes.use('/:username/seguindo', seguidoRoute);
+
 
 module.exports = routes;
